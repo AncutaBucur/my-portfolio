fdf107305085f1e9c404f449c75fcf04aea4aa2e
AncutaBucur <ancutabucur@gmail.com>
2025-12-18 16:08:19 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+document.getElementById('year').textContent = new Date().getFullYear();
+
+document.getElementById('button-about').addEventListener('click', function() {
+    alert('Not implemented yet.');
+});
+
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+
+    if(dots.style.display === "none"){
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more";
+        moreText.style.display = "none";
+    }
+    else{
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+    }
+}
